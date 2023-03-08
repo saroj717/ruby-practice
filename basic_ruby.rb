a8bb57bac275fcbf0bbc6793235aa41ea6886b11
Saroj Shrestha <sarojshrestha@Sarojs-MacBook-Air.local>
2023-03-08 09:39:11 -0700

    my first commit

---
+++ basic_ruby.rb
@@ -0,0 +1,13 @@
+character_name1 = "Saroj"
+character_age1 = "40"
+character_name2 = "Sabina"
+character_age2 = "29"
+character_name3 = "Sarina"
+character_age3 = "6"
+
+
+puts "My name is " + character_name1 + ". I am " + character_age1 + " years old."
+puts "My wife name is " + character_name2 + ". She is " + character_age2 + " years old."
+puts "My daughter name is " +  character_name3 + ". She is " + character_age3 + " years old."
+
+
