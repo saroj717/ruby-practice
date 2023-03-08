027bb2e94ea2523e628274bd4de314ebda5f6d10
Saroj Shrestha <sarojshrestha@Sarojs-MacBook-Air.local>
2023-03-08 10:43:19 -0700

    Mad Libs Game

---
+++ file2.rb
@@ -0,0 +1,23 @@
+
+
+
+puts "******** MAD LIBS GAME *********"
+
+puts "Enter a color: "
+color = gets.chomp
+
+puts "Enter plural_noun: "
+plural_noun = gets.chomp
+
+puts "Enter a celebrity: "
+celebrity = gets.chomp
+
+
+
+puts "Roses are #{color}."
+puts "#{plural_noun} are blue."
+puts "I love #{celebrity}."
+
+
+
+puts "~~~~~~~~%%%%%%%%%%~~~~~~~~~~~~"
