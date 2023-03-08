dc10847828f3be1026cceefaaae43c7da1be3092
Saroj Shrestha <sarojshrestha@Sarojs-MacBook-Air.local>
2023-03-08 10:22:12 -0700

    My Calculator

---
+++ file1.rb
@@ -0,0 +1,14 @@
+puts "Enter the value: "
+num1 = gets.chomp().to_f
+puts "Enter the value: "
+num2 = gets.chomp().to_f
+
+
+puts "Addition: " + (num1 + num2).to_s
+puts "Subtraction: " + (num1 - num2).to_s
+puts "Multiplication: " + (num1 * num2).to_s
+puts "Division: " + (num1 / num2).to_s
+puts "Power: " + (num1 ** 2).to_s
+puts "Square Root num1: " + Math.sqrt(num1).to_s
+puts "Square Root num2: " + Math.sqrt(num2).to_s
+
