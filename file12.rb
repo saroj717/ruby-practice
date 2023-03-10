36267476cd335342da7fef43b6d3c982bb7fe501
Saroj Shrestha <sarojshrestha@Sarojs-MacBook-Air.local>
2023-03-10 16:39:26 -0700

    Makeing Triangle

--- file12.rb
+++ file12.rb
@@ -1,5 +1,45 @@
-'A'.upto('Z') do|i|
-puts " #{i}"
+# 'A'.upto('Z') do|i|
+# puts " #{i}"
+# end
+
+# puts "Happy New Year!!!"
+
+
+# factor = 2
+# [1,2,3,4,5,6,7,8,9,10].each do |i|
+#     puts "#{i} x #{factor} = #{i*factor}\n" 
+#     puts "#{i.to_f} / #{factor} = #{i.to_f/factor}\n"
+#     puts "#{i} + #{factor} = #{i+factor}\n"  
+#     puts "#{i} - #{factor} = #{i-factor}"
+    
+   
+# end
+# puts i
+
+
+print "Enter Number for Rows", ":"
+rows = gets.chomp.to_i
+for row in 0..rows
+    row.times{print"*"}
+    puts
 end
 
-puts "Happy New Year!!!"
+# print "Enter Number for Rows", ":"
+# rows = gets.chomp.to_i
+# for row in 0..rows
+#    (rows-row).times{print"*"}
+#     puts
+# end
+
+# print "Enter Number for Rows", ":"
+# rows = gets.chomp.to_i
+
+# for row in 0..rows
+    
+#     row.times{print"*"}
+#     (rows-row).times{print" "}
+#     puts
+# end
+
+
+
---
+++ file13.rb
@@ -0,0 +1,6 @@
+puts "Enter the number of rows: "
+rows = gets.chomp.to_i
+for rows in 0..rows
+    rows.times {print "^"}
+    puts
+end
