37be1d61947f86f7500327c4be1b345951495365
Saroj Shrestha <sarojshrestha@Sarojs-MacBook-Air.local>
2023-03-14 07:18:06 -0600

    Some additional in excersize

--- ruby21.rb
+++ ruby21.rb
@@ -31,3 +31,80 @@ def us_states(*states)
 end
 us_states("Alaska","Texas", "California")
 
+
+#Ruby sort method
+array= [1,3,5,6,0,5,6,7]
+array.sort!
+print array
+
+#Ruby method parameters and arguments
+def division(i)
+ puts i / 2
+end
+division(3)
+
+
+#Ruby block
+5.times do
+    puts "My name is saroj"
+end
+
+5.times {print "My name is saroj   "}
+
+
+#Ruby symbols
+family = {
+:father => "saroj",
+:mother => "sabina",
+:daughter => "sarina"
+}
+
+puts family[:daughter]
+
+
+#Ruby .select method
+
+exam_marks = {
+    saroj: 70,
+    sarina: 90,
+    sabina: 50
+}
+
+exam_marks.select {|name, marks| marks < 60}
+
+
+#Ruby .each_key & .each_value
+saroj_shrestha = {
+    age: 40,
+    wife: "sabina",
+    daughter: "sarina"
+}
+
+saroj_shrestha.each_key {|key| puts key}
+saroj_shrestha.each_value {|value| puts value}
+
+
+Utah = {
+    capital_city: "Satl_Lake_City",
+    Ski_city: "Park_City",
+    World_class_ski: 4,
+    weather: "dry"
+}
+
+Utah.each_value {|value | puts value}
+Utah.each_key {|key| puts key}
+
+
+shrestha_family = {
+    husband: "Saroj",
+    wife: "Sabina",
+    daughter: "Sarina"
+}
+
+shrestha_family.each_key do|key|
+puts key
+end
+
+shrestha_family.each_value do|value|
+puts value
+end
