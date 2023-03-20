5bfcd01cd8140f6072f6457a42d2174dd92d12d9
Saroj Shrestha <sarojshrestha@Sarojs-MacBook-Air.local>
2023-03-20 08:46:13 -0600

    Ruby Variables

---
+++ file24.rb
@@ -0,0 +1,8 @@
+#Ruby Variables
+puts "Types of Variales"
+
+puts "Local Variable" # This variable can be accessed locally. It alwasys start with lowercase letter.
+puts "Class Variable" # This variable starts with double @@
+puts "Instance Variable" # This variable starts wiht single @
+puts "Global Variable"   #This variable starts wiht $
+puts "Constants" # This alwasy starts with uppercase letter and it is always constant, that means it is neber changed.
